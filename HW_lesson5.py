0b30d03462a1be6f651508d68658e9bd95e85274
Vlada <vladonkapol@gmail.com>
2025-10-12 18:23:43 +0300

    HW_lesson5

---
+++ HW_lesson5.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+img = cv2.imread('images/img9.jpg')
+#img = cv2.resize(img, (img.shape[1]//2, img.shape[0]//2))
+img_copy = img.copy()
+img = cv2.GaussianBlur(img, (5, 5), 2)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+lower = np.array([0, 33, 21])
+upper = np.array([179, 255, 255])
+
+mask = cv2.inRange(img, lower, upper)
+img = cv2.bitwise_and(img, img, mask=mask)
+
+contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+for cnt in contours:
+    area = cv2.contourArea(cnt)
+    if area > 200:
+        x, y, w, h = cv2.boundingRect(cnt)
+
+        perimeter = cv2.arcLength(cnt, True)
+        M = cv2.moments(cnt)
+
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+
+        aspect_ratio = round(w / h, 2)
+        compactness = round((4 * np.pi * area) / (perimeter ** 2), 2)
+        approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
+        if len(approx) == 4:
+            shape = "square"
+        elif len(approx) == 3:
+            shape = "triangle"
+        elif len(approx) == 8:
+            shape = "circle"
+        elif len(approx) > 5 and len(approx) != 8:
+            shape = "star"
+        else:
+            shape = "something else"
+
+        cv2.putText(img_copy, f'S: {area}, P: {perimeter}', (x-30, y-45), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+        cv2.circle(img_copy, (cx, cy), 5, (0, 0, 255), -1)
+        cv2.putText(img_copy, f'AR: {aspect_ratio}, C:{compactness}', (x-30, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+        cv2.putText(img_copy, f'shape: {shape}', (x-30, y-25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+        cv2.rectangle(img_copy, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+
+cv2.imshow("image", img)
+cv2.imwrite("result.jpg", img_copy)
+cv2.imshow("mask", img_copy)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
